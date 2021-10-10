7726084e8a2944ae2c873a5510fa3dcb77cf10aa
fayyozbobokulov <b.fayyoz2000@gmail.com>
2021-10-11 00:38:09 +0500

    1. Server is created, 2. Modules are created, 3. Repositories are created, 4. Controller are created only for user, 5. User route is created, 6. Postgres DB is Connected

---
+++ src/models/user.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from "typeorm";
+import { Post } from "./post";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  firstName!: string;
+
+  @Column()
+  lastName!: string;
+
+  @Column()
+  email!: string;
+
+  @OneToMany((_type) => Post, (post: Post) => post.user)
+  posts!: Array<Post>;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
